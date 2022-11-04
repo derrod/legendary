4c99bf898708b882b2e9e667c7de068bccebe9df
derrod <xlnedder@gmail.com>
2022-11-04 14:54:30 +0100

    setup.py: Bump minimum python version to 3.9

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup
 
 from legendary import __version__ as legendary_version
 
-if sys.version_info < (3, 8):
-    sys.exit('python 3.8 or higher is required for legendary')
+if sys.version_info < (3, 9):
+    sys.exit('python 3.9 or higher is required for legendary')
 
 with open("README.md", "r") as fh:
     long_description_l = fh.readlines()
@@ -47,11 +47,10 @@ setup(
     description='Free and open-source replacement for the Epic Games Launcher application',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft',
